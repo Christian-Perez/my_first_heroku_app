173a00fdbe39017a3ce8052a7edd80f6bef641e4
Christian Perez <chris.dillon92@gmail.com>
2016-07-19 12:15:35 -0700

    connects to github & heroku + seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puppies = Puppy.create([{name: 'Rick', age: 2, breed: 'kangaroo'}, {name: 'Paul', age: 4, breed: 'dog'}, {name: 'Charlie', age: 3, breed: 'chicken'}])
